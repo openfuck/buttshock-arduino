c348b61b55f9243bd9561a8c5d0746f3b6249f20
Mister Fred <justmrfred@gmail.com>
2015-07-13 12:27:54 +0100

    First version for git

---
+++ Venerate.cpp
@@ -0,0 +1,200 @@
+// Venerate: A library for communicating to the ET312 box via serial
+//
+// tip is rx (yellow), ring is tx (green), body in gnd
+// you must use a ttl to rs232 converter and hardware serial
+//
+// This arduino code is based off the et312-perl code but with the
+// addition of some new methods that were not known before (figured out
+// by trial-and-error watching all the available memory locations)
+//
+// June 2015
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#elif defined(SPARK)
+#include "application.h"
+#endif
+
+#include "Venerate.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "EEPROM.h" // store the last mod byte in case we lose power
+#endif
+
+Venerate::Venerate(byte boxid)
+{
+    _debug = 0;
+    _state = 0;
+    _boxid = boxid;
+}
+
+void Venerate::begin(Stream &serial)
+{
+    _serial = &serial;
+    _serial->setTimeout(5UL);
+}
+
+void Venerate::setdebug(Stream &debugserial, byte debug) {
+    _debug = debug;
+    _debugserial = &debugserial;
+}
+
+void Venerate::setmod(byte mod) {
+    _mod = mod;
+}
+
+boolean Venerate::isconnected(void) {
+    return (_state == 1);
+}
+
+// Send a series of bytes to the box and return the result, based
+// on the code from et312-perl
+
+int Venerate::cp(byte msg[], byte n, byte reply[]) {
+    byte sum = 0;
+    for (byte i = 0; i < n; i++) {
+        byte c = msg[i];
+        sum += c; // overflow expected and ok
+        c ^= _mod;
+        if (_debug>1) {
+            Serial.print(c, HEX);
+            Serial.print(" ");
+        }
+        _serial->write((byte)c);
+    }
+    if (n > 1) {
+        if (_debug>1) _debugserial->print((byte)(sum ^ _mod), HEX);
+        _serial->write((byte)sum ^ _mod);
+    }
+    if (_debug>1) _debugserial->println(" tx");
+    _serial->flush();
+    // In perl we wait for 10+10*1 milliseconds  20ms only!
+#if defined (SPARK)
+    byte bread = _serial->readBytes((char *)reply, maxrxbytes);
+#else
+    byte bread = _serial->readBytes(reply, maxrxbytes);
+#endif
+
+    if (bread <1) {
+        if (_debug) _debugserial->println("no rx");
+        _state = 0;
+    } else if (_debug>1) {
+        for (int i = 0; i < bread; i++) {
+            _debugserial->print(reply[i], HEX);
+            _debugserial->print(" ");
+        }
+        _debugserial->println(" rx");
+    }
+    return bread;
+}
+
+// Return the byte at the memory address or -1 if error
+
+int Venerate::getbyte(int n) {
+    byte reply[maxrxbytes];
+    byte msg[3];
+
+    msg[0] = 0x3c;
+    msg[1] = (byte)(n >> 8);
+    msg[2] = (byte)(n & 255);
+    int count = Venerate::cp(msg, 3, reply);
+    if (count < 3) return -1; // got to be 3 chars
+    if (reply[0] != 0x22) return -1; // first is 0x22
+    byte sum = reply[0] + reply[1]; // with valid checksum, allow overflow
+    if (sum != reply[2]) return -1;
+    return reply[1];
+}
+
+// Send a byte to a memory address, false if error
+
+boolean Venerate::setbyte(int n, int b) {
+    byte reply[maxrxbytes];
+    byte msg[4];
+
+    msg[0] = 0x4d;
+    msg[1] = (byte)(n >> 8);
+    msg[2] = (byte)(n & 255);
+    msg[3] = (byte)(b);
+    int count = Venerate::cp(msg, 4, reply);
+    if (count < 1) return false; // got to be 1 chars
+    if (reply[0] != 0x06) return false; // success is a 6
+    return true;
+}
+
+// hello, hello, good to be back
+
+boolean Venerate::hello()
+{
+    if (_state != 0) {
+        if (_debug) _debugserial->println("State !0");
+        return Venerate::isconnected();
+    }
+    byte rx[maxrxbytes];
+
+    if (_debug) _debugserial->println("tx hello");
+    int s = 0;
+    for (int i = 0; i < 10; i++) {
+        byte send[] = {0x00};
+        _mod = 0;
+        int chars = Venerate::cp(send, 1, rx);
+        if (chars > 0 && rx[0] == 0x07) {
+            s++;
+            if (s > 3) break;
+        } else {
+            s = 0;
+        }
+    }
+
+    if (s > 3) {
+        if (_debug) _debugserial->println("rx hello");
+        byte send[] = {0x2f, 0x00};
+        _mod = 0;
+        int chars = Venerate::cp(send, 2, rx);
+        int sum = rx[0] + rx[1];
+        if (sum > 256) sum -= 256;
+        if (chars < 3 || rx[0] != 0x21 || sum != rx[2]) {
+            if (_debug) _debugserial->println("no sync");
+        } else {
+            _mod = rx[1] ^ 0x55;
+            if (_debug) {
+                _debugserial->print(rx[1], HEX);
+                _debugserial->println("=mod");
+            }
+            EEPROM.write(_boxid, _mod);
+        }
+    } else {
+        _mod = EEPROM.read(_boxid);
+        if (_debug) {
+            _debugserial->print(_mod, HEX);
+            _debugserial->println(" mod try");
+        }
+        int s = 0;
+        for (int i = 0; i < 10; i++) {
+            byte send[] = {0x00};
+            int chars = Venerate::cp(send, 1 , rx);
+            if (chars > 0 && rx[0] == 0x07) {
+                s++;
+                if (s > 3) break;
+            } else {
+                s = 0;
+            }
+        }
+        if (s < 1) {
+            if (_debug) _debugserial->println("fail");
+            return Venerate::isconnected();
+        }
+    }
+    // just a test memory get
+    int y = Venerate::getbyte(ETMEM_knoba);
+    if (y < 0) {
+        if (_debug) _debugserial->println("fail");
+    } else {
+        if (_debug) {
+            _debugserial->print(y, HEX);
+            _debugserial->println("=knoba");
+        }
+        _state = 1;
+    }
+    return Venerate::isconnected();
+}
+
---
+++ Venerate.h
@@ -0,0 +1,134 @@
+#ifndef Venerate_h
+#define Venerate_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#elif defined(SPARK)
+#include "application.h"
+#endif
+
+#define maxrxbytes 4
+
+#define ETMEM_levela 0x4009
+#define ETMEM_levelb 0x400E
+#define ETMEM_panellock 0x400F
+#define ETMEM_knoba 0x4064
+#define ETMEM_knobb 0x4065
+#define ETMEM_knobma 0x4061
+#define ETMEM_pushbutton 0x4070
+
+// push panel buttons: 
+// (from protocol analysis when box switches to 'user1' mode 20121116, what other things can we push?)
+// 4 used when setting mode
+// 5 not sure, ramps 0-100
+// 15 makes display say 'adjust?'
+// 16 used when setting mode
+// 17 split mode
+
+#define ETBUTTON_ok 3
+#define ETBUTTON_setmode 4
+#define ETBUTTON_up 8
+#define ETBUTTON_down 9
+#define ETBUTTON_menu 10
+#define ETBUTTON_split 12
+#define ETBUTTON_lockmode 16
+#define ETBUTTON_splitmode 17
+
+#define ETMEM_modesplit 0x4078
+
+#define ETMODE_waves 0x76
+#define ETMODE_stroke 0x77
+#define ETMODE_climb 0x78
+#define ETMODE_combo 0x79
+#define ETMODE_intense 0x7A
+#define ETMODE_rhythm 0x7B
+#define ETMODE_audio1 0x7C
+#define ETMODE_audio2 0x7D
+#define ETMODE_audio3 0x7E
+#define ETMODE_split 0x7F
+#define ETMODE_random1 0x80
+#define ETMODE_random2 0x81
+#define ETMODE_toggle 0x82
+#define ETMODE_orgasm 0x83
+#define ETMODE_torment 0x84
+#define ETMODE_phase1 0x85
+#define ETMODE_phase2 0x86
+#define ETMODE_phase3 0x87
+#define ETMODE_user1 0x88
+#define ETMODE_user2 0x89
+#define ETMODE_user3 0x8A
+#define ETMODE_user4 0x8B
+#define ETMODE_user5 0x8C
+
+#define ETMEM_mode 0x407B
+
+// MA knob is strange, we can get/set the value 0-100 as per the other knobs, but in
+// real life the range is limited depending on the mode.  There must be a memory
+// location that specifies the allowed range of the knob, we'll also need to set it
+// when we change mode (it doesn't change if mode changes and panel is locked)
+// So location 4087 is the max MA, 4086 is the min MA (through watching all locations!)
+
+#define ETMEM_knobmamin 0x4086
+#define ETMEM_knobmamax 0x4087
+#define ETMEM_gatea 0x4090
+#define ETMEM_timeona 0x4098
+#define ETMEM_timeoffa 0x4099
+#define ETMEM_timeopta 0x409A 
+#define ETMEM_ramp 0x4006 // 0-255 also ramp at 400b, 409c, 419c
+#define ETMEM_glevela 0x40A5
+#define ETMEM_glevelmina 0x40A6
+#define ETMEM_glevelmaxa 0x40A7
+#define ETMEM_glevelratea 0x40A8
+#define ETMEM_glevelopta 0x40AB
+#define ETMEM_freqa 0x40AE
+#define ETMEM_freqmaxa 0x40AF
+#define ETMEM_freqmina 0x40B0
+#define ETMEM_freqopta 0x40B5
+#define ETMEM_widtha 0x40B7
+#define ETMEM_widthmina 0x40B8
+#define ETMEM_widthmaxa 0x40B9
+#define ETMEM_widthopta 0x40BE
+#define ETMEM_gateb 0x4190
+#define ETMEM_timeonb 0x4198
+#define ETMEM_timeoffb 0x4199
+#define ETMEM_timeoptb 0x419A
+#define ETMEM_glevelb 0x41A5
+#define ETMEM_glevelminb 0x41A6
+#define ETMEM_glevelmaxb 0x41A7
+#define ETMEM_glevelrateb 0x41A8
+#define ETMEM_gleveloptb 0x41AB
+#define ETMEM_freqb 0x41AE
+#define ETMEM_freqmaxb 0x41AF
+#define ETMEM_freqminb 0x41B0
+#define ETMEM_freqoptb 0x41B5
+#define ETMEM_widthb 0x41b7
+#define ETMEM_widthminb 0x41B8
+#define ETMEM_widthmaxb 0x41B9
+#define ETMEM_widthoptb 0x41BE
+#define ETMEM_powerlevel 0x41F4
+#define ETMEM_ramptime 0x41F9
+#define ETMEM_knobmaset 0x420d
+
+class Venerate
+{
+ public:
+    Venerate(byte boxid);
+    void begin(Stream &serial);
+    void setdebug(Stream &debugserial, byte debug);
+    void setmod(byte mod);
+    boolean isconnected(void);
+    int cp(byte msg[], byte n, byte reply[]);
+    int getbyte(int n);
+    boolean setbyte(int n, int b);
+    boolean hello();
+ private:
+    byte _boxid;
+    Stream* _serial;
+    Stream* _debugserial;
+    byte _debug;
+    byte _mod;
+    byte _state;
+};
+
+#endif
+
